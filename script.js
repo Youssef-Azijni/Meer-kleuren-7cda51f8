dd476d509f2b6a9c311a3544e7051e5d5bdf9ecf
PickleRick1488 <2104485@talnet.nl>
2020-02-14 12:01:09 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,13 @@
+function changecolorh1(){
+    var diffcolorh1 = document.getElementById("color").value;
+    document.querySelector("h1").style.color = diffcolorh1 ;
+}
+
+function changecolorpage(){
+    var diffcolorpage = document.getElementById("color").value;
+    document.querySelector("body").style.backgroundColor = diffcolorpage ;
+}
+
+
+
+
